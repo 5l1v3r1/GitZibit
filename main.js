d98be159a03bbd48e453990fd69a5bacc396644e
ScriptSmith <ScriptSmith@users.noreply.github.com>
2016-03-12 18:57:08 +1000

    Implemented initial Material Design Homepage

--- main.js
+++ main.js
@@ -5,39 +5,3 @@ language = []
 forked = true
 size = ["0kb","1kb","10kb","100kb","1mb","5mb","20mb","100mb","500mb","1gb"]
 query = ""
-
-$(function() {
-   $( ".slider-ui" ).slider({
-     value:5,
-     min: 0,
-     max: 10,
-     step: 1,
-   });
- });
-
- $(function() {
-  $( "#stars-ui" ).slider({
-    range: true,
-    min: 0,
-    max: 9,
-    values: [4, 6]
-  });
-});
-
-$(function() {
-  $( "#language-ui" ).menu();
-});
-
-$(function() {
-  $("#newness").val("< " + date[$( "#newness-ui" ).slider( "value")+1]);
-  $("#newness-ui").slider({
-    slide: function( event, ui ) {
-      $("#newness").val("< " + date[$( "#newness-ui" ).slider( "value")]);
-      console.log($( "#newness-ui" ).slider( "value"));
-    }
-  })
-})
-
-function resizeInput() {
-    $(this).attr('size', $(this).val().length);
-}
