5b5989401f85fbc17665a326af5c6af090cf1023
ScriptSmith <ScriptSmith@users.noreply.github.com>
2016-03-14 21:17:54 +1000

    Added more to pages

--- main.js
+++ main.js
@@ -37,32 +37,58 @@ data =
   }
 ]
 
-$(function(){
-  for (var i=0; i < data.length; i++){
-    $(".mdl-grid").append('<div class="mdl-cell mdl-cell--4-col"> \
+function shuffle(o) {
+	for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+	return o;
+};
+
+function insertCard(results, gridName){
+  for (var i=0; i < results.length; i++){
+    $(gridName).append('<div class="mdl-cell mdl-cell--4-col mdl-cell--stretch"> \
       <div class="demo-card-event mdl-card mdl-shadow--4dp">\
         <div class="mdl-card__title">\
-          <img class="material-icons card-avatars mdl-list__item-avatar" src="' + data[i]["avatar"] + '" width="40px" />\
+          <img class="material-icons card-avatars mdl-list__item-avatar" src="' + results[i]["owner"]["avatar_url"] + '" width="40px" />\
           <h4>\
-            ' + data[i]["user"] + ' / ' + data[i]["project"] + '\
+            <a href="https://github.com/' + results[i]["owner"]["login"] + '">' + results[i]["owner"]["login"] + ' /</a> <a href="' + results[i]["html_url"] + '">' + results[i]["name"] + '</a>\
             <div class="mdl-card__subtitle-text">\
-              ' + data[i]["date"] + '\
+              ' + results[i]["created_at"].slice(0,10) + '\
             </div>\
           </h4>\
         </div>\
         <div class="mdl-card__supporting-text">\
-          ' + data[i]["description"] + '\
+          ' + results[i]["description"] + '\
         </div>\
         <div class="mdl-card__actions mdl-card--border">\
           <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">\
             View More\
           </a>\
           <div class="mdl-layout-spacer"></div>\
-          <i class="material-icons card-icons">star</i>' + data[i]["stars"] + '\
-          <i class="material-icons card-icons">file_download</i>' + data[i]["size"] + '\
+          <div style="cursor: pointer" onclick="window.open(' + "'" + results[i]["html_url"] + '/stargazers' + "'" + ')"><i class="material-icons card-icons">star</i></div>' + results[i]["stargazers_count"] + '\
+          <div style="cursor: pointer" onclick="window.open(' + "'" + results[i]["html_url"] + '/archive/master.zip' + "'" + ')"><i class="material-icons card-icons">file_download</i></div>' + results[i]["size"] + 'KB\
         </div>\
       </div>\
     </div>\
     ')
   }
-})
+}
+
+$.get( "https://api.github.com/search/repositories", { q: "created:>=2016-02-14", sort: "stars" }, function( data ) {
+  results = shuffle(data["items"].slice(0,parseInt(data["items"].length / 3))).slice(0,6)
+  insertCard(results, "#main-top")
+});
+
+$.get( "https://api.github.com/search/repositories", { q: "created:>=2016-03-11", sort: "stars" }, function( data ) {
+  results = data["items"]
+  insertCard(results, "#hot")
+  results = shuffle(data["items"].slice(0,parseInt(data["items"].length / 3))).slice(0,6)
+  insertCard(results, "#main-hot")
+});
+
+$.get( "https://api.github.com/search/repositories", { q: "stars:10..50", sort: "updated" }, function( data ) {
+  results = shuffle(data["items"].slice(0,parseInt(data["items"].length / 3))).slice(0,6)
+  insertCard(results, "#main-obscure")
+});
+
+$.get( "https://api.github.com/search/repositories", {q: sort: "updated" }, function( data ) {
+  insertCard(data["items"], "#new")
+});
